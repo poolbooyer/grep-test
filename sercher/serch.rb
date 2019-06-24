dde7d29518b193edb1dd80bb049df3170a7a3c97
poolbooyer <ryou3018@gmail.com>
2019-06-24 23:38:26 +0900

    refact result output style

--- sercher/serch.rb
+++ sercher/serch.rb
@@ -2,12 +2,17 @@ def sercher
   head="grep "
   option=ARGV[0]+" "+ARGV[1]+" "
   path=ARGV.last
+  
   if path=="all" then
     path="data/"
   end
+  
   cmd=head+option+path+"*"
   result=`#{cmd}`
-  fname="./result.txt"
+  
+  puts "input file name"
+  fname = STDIN.gets.chomp+".txt"
+  
   File.open(fname, "a") do |f|
     f.puts(result)
   end
