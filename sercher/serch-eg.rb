c5343e91409be67a560fbe99922bd69c666b5f87
poolbooyer <ryou3018@gmail.com>
2019-06-27 23:59:31 +0900

    support pattern of not found

--- sercher/serch-eg.rb
+++ sercher/serch-eg.rb
@@ -1,4 +1,3 @@
-fi=Dir::entries(ARGV[0])
 def read_file(path)
   r_data=[]
   r_data[0]=path
@@ -6,12 +5,6 @@ def read_file(path)
     r_data[1]=f.read
   end
   return r_data
-end
-data=[]
-for i in 0..(fi.length-1) do
-  if fi[i].length>2 then
-    data.push(read_file(ARGV[0]+fi[i]))
-  end
 end
 def serch_infile(target,query)
   res=[] 
@@ -29,16 +22,28 @@ def serch_each(data,query)
     puts line[0]
     target=line[1].split("\n")
     res=serch_infile(target,query)
-    
+    count=0
     for i in 0..(res.length-1) do
-     if res[i]==true then
+      if res[i]==true then
         puts "L#{i+1}: #{target[i]}"
+      else
+        count+=1
       end
-      
+    end
+    if count==(res.length) then
+      puts "Not found"
     end
     puts ""
   end
 end
+fi=Dir::entries(ARGV[0])
+data=[]
+for i in 0..(fi.length-1) do
+  if fi[i].length>2 then
+    data.push(read_file(ARGV[0]+fi[i]))
+  end
+end
+
 query=ARGV[1]
 
 serch_each(data,query)
